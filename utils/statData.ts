530412b0f8b0228889a0dd150ef7094cda22af52
rikuyu <rikuyu5884@gmail.com>
2025-04-08 19:22:02 +0900

    add japanese data

---
+++ utils/statData.ts
@@ -0,0 +1,8 @@
+export const statData = new Map<string, string>([
+  ["hp", "HP"],
+  ["attack", "こうげき"],
+  ["speed", "すばやさ"],
+  ["defense", "ぼうぎょ"],
+  ["special-attack", "とくこう"],
+  ["special-defense", "とくぼう"],
+]);
