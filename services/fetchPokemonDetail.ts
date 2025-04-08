12a087ce33057aaae06fe93039c0c7fff401aa3c
rikuyu <rikuyu5884@gmail.com>
2025-04-08 21:25:18 +0900

    fetch pokemon detail function

---
+++ services/fetchPokemonDetail.ts
@@ -0,0 +1,41 @@
+import { getPokemonDetailUrl } from "@/api/endpoints";
+import { PokemonDetail, PokemonStat, PokemonType, RawPokemonStat, RawPokemonType } from "@/types/pokemon";
+import { typeData } from "@/utils/typeData";
+import { statData } from "@/utils/statData";
+import { pokemonData } from "@/utils/pokemonData";
+
+export const fetchPokemonDetail = async (index: number): Promise<PokemonDetail> => {
+  const url = getPokemonDetailUrl(index);
+
+  return fetch(url)
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(`(${res.status}): Failed to fetch Pokémon Detail`);
+      }
+      return res.json();
+    })
+    .then((data) => {
+      const pokemonDetail: PokemonDetail = {
+        index: data.id,
+        name: pokemonData.get(data.name) ?? "unknown",
+        height: data.height,
+        weight: data.weight,
+        types: data.types.map((v: RawPokemonType) => {
+          const type: PokemonType = {
+            name: typeData.get(v.type.name)?.label ?? "unknown",
+            color: typeData.get(v.type.name)?.color.toString(16) ?? "fff",
+          };
+          return type;
+        }),
+        stats: data.stats.map((v: RawPokemonStat) => {
+          const stat: PokemonStat = {
+            name: statData.get(v.stat.name) ?? "unknown",
+            status: v.base_stat,
+          };
+          return stat;
+        }),
+      };
+      return pokemonDetail;
+    })
+    .catch();
+};
