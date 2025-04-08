b3b7afbf03e637218d51425cca0efcce3e1446bb
rikuyu <rikuyu5884@gmail.com>
2025-04-08 21:25:08 +0900

    fetch pokemon detail

--- app/pokemon/[id].tsx
+++ app/pokemon/[id].tsx
@@ -1,17 +1,48 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
 import { useLocalSearchParams } from "expo-router";
+import { useFetch } from "@/hooks/useFetch";
+import { PokemonDetail } from "@/types/pokemon";
+import { fetchPokemonDetail } from "@/services/fetchPokemonDetail";
 
-export default function PokemonDetail() {
+export default function PokemonProfile() {
   const {id} = useLocalSearchParams();
 
+  const {
+    data,
+    isLoading,
+    hasError,
+  } = useFetch<PokemonDetail, number>(Number(id), fetchPokemonDetail);
+
+  console.log(data);
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large"/>
+      </View>
+    );
+  }
+
+  if (hasError) {
+    return (
+      <View style={styles.container}>
+        <Text>Error</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={{
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-    }}>
-      <Text>Pokemon ID: {id}</Text>
+    <View style={styles.container}>
+      <Text>{data?.name}</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
