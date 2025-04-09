3254a639c95d3b704e24a59696a8f0c0f8bcce65
rikuyu <rikuyu5884@gmail.com>
2025-04-09 19:06:32 +0900

    define type

---
+++ types/pokemon.ts
@@ -0,0 +1,43 @@
+export type PokemonListResponseItem = {
+  name: string;
+}
+
+export type PokemonListItem = PokemonListResponseItem & {
+  index: number;
+}
+
+export type PokemonDetail = {
+  index: number;
+  name: string;
+  height: number;
+  weight: number;
+  types: PokemonType[];
+  stats: PokemonStat[];
+}
+
+export type PokemonStat = {
+  name: string;
+  status: number;
+};
+
+export type PokemonType = {
+  name: string;
+  color: string;
+}
+
+export type RawPokemonType = {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+};
+
+export type RawPokemonStat = {
+  base_stat: number;
+  effort: number;
+  stat: {
+    name: string;
+    url: string;
+  };
+};
