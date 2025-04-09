7270f1f8a6261e2a4890f8c6dd363bac95209d25
rikuyu <rikuyu5884@gmail.com>
2025-04-09 19:06:53 +0900

    fix endpoint

--- api/endpoints.ts
+++ api/endpoints.ts
@@ -6,5 +6,4 @@ export const getPokemonListUrl = (offset: number, limit: number) =>
 export const getPokemonImage = (index: number) =>
   `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index}.png`;
 
-export const getPokemonDetailUrl = (index: number) =>
-  `https://pokeapi.co/api/v2/pokemon-species/${index}`;
+export const getPokemonDetailUrl = (index: number) => `${POKEMON_URL}/${index}`;
