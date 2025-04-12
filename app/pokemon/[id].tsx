cf76cc97b29f75dc29a3c70d4463e457776db804
rikuyu <rikuyu5884@gmail.com>
2025-04-13 00:02:38 +0900

    Pokemon Profile

--- app/pokemon/[id].tsx
+++ app/pokemon/[id].tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import { ActivityIndicator, Image, StyleSheet, Text, useWindowDimensions, View } from "react-native";
 import { useLocalSearchParams } from "expo-router";
 import { useFetch } from "@/hooks/useFetch";
 import { PokemonDetail } from "@/types/pokemon";
 import { fetchPokemonDetail } from "@/services/fetchPokemonDetail";
 import { usePokemonHeaderOptions } from "@/hooks/usePokemonHeaderEffect";
+import { getPokemonImage } from "@/api/endpoints";
+import GradientOrSolidBackground from "@/components/GradientOrSolidBackground";
+import PokemonProfileTitle from "@/components/PokemonProfileTitle";
+import StatSection from "@/components/StatSection";
+import TypeSection from "@/components/TypeSection";
+import PhysicalSection from "@/components/PhysicalSection";
 
 export default function PokemonProfile() {
   const {id} = useLocalSearchParams();
@@ -16,6 +22,9 @@ export default function PokemonProfile() {
   } = useFetch<PokemonDetail, number>(Number(id), fetchPokemonDetail);
 
   usePokemonHeaderOptions(data, isLoading, isLoading);
+
+  const {height} = useWindowDimensions();
+  const imgSize = height / 6;
 
   if (isLoading) {
     return (
@@ -35,7 +44,22 @@ export default function PokemonProfile() {
 
   return (
     <View style={styles.container}>
-      <Text>{data?.name}</Text>
+      <GradientOrSolidBackground colors={data?.types.map((type) => type.color)}>
+        <Image
+          style={{height: imgSize, width: imgSize}}
+          source={{uri: getPokemonImage(data?.index)}}
+        />
+      </GradientOrSolidBackground>
+      <View style={{height: 12}}/>
+      <PokemonProfileTitle title={data?.name}/>
+      <View style={{height: 12}}/>
+      <View style={{flex: 4, alignItems: "center"}}>
+        <TypeSection types={data?.types}/>
+        <View style={{height: 12}}/>
+        <PhysicalSection height={data?.height} weight={data?.weight}/>
+        <View style={{height: 16}}/>
+        <StatSection stats={data?.stats}/>
+      </View>
     </View>
   );
 }
@@ -43,6 +67,16 @@ export default function PokemonProfile() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
+    backgroundColor: "#fc5353",
+  },
+  linearGradient: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  rowCenter: {
+    flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
   },
