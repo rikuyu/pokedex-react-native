1d90d886a2a24b319d6c2b58c859116b1e1b161f
rikuyu <rikuyu5884@gmail.com>
2025-04-17 00:20:42 +0900

    scroll animation header

--- components/MyPageHeader.tsx
+++ components/MyPageHeader.tsx
@@ -1,36 +1,74 @@
 import React from "react";
-import Animated, {
-  Extrapolation,
-  interpolate,
-  SharedValue,
-  useAnimatedScrollHandler,
-  useAnimatedStyle,
-  useSharedValue,
-} from "react-native-reanimated";
+import Animated, { Extrapolation, interpolate, SharedValue, useAnimatedStyle } from "react-native-reanimated";
+import { ImageStyle, StyleSheet, useWindowDimensions, View } from "react-native";
+import { ViewStyle } from "react-native/Libraries/StyleSheet/StyleSheetTypes";
 
 type Props = {
+  positionStyle: ViewStyle;
   scrollOffset: SharedValue<number>;
 }
 
-export default function MyPageHeader({scrollOffset}: Props) {
-
-  const scrollY = useSharedValue(0);
-  const scrollHandler = useAnimatedScrollHandler((event) => {
-    scrollY.value = event.contentOffset.y;
-  });
+export default function MyPageHeader({positionStyle, scrollOffset}: Props) {
+  const {width} = useWindowDimensions();
+  const imageSize = width / 5;
 
   const animatedHeaderStyle = useAnimatedStyle(() => ({
     height: interpolate(
-      scrollY.value,
-      [0, 200],
-      [200, 40],
+      scrollOffset.value,
+      [0, 160],
+      [160, 50],
       Extrapolation.CLAMP,
     ),
   }));
 
-  return (
-    <Animated.View style={[animatedHeaderStyle]}>
+  const animatedProfileImageStyle = useAnimatedStyle(() => {
+    const size = interpolate(
+      scrollOffset.value,
+      [0, 160],
+      [imageSize, imageSize * 2 / 3],
+      Extrapolation.CLAMP,
+    );
+    const radius = size / 2;
+    const bottom = -1 * size / 2;
 
+    return {
+      width: size,
+      height: size,
+      borderRadius: radius,
+      bottom,
+    };
+  });
+
+  return (
+    <Animated.View style={[styles.headerContainer, positionStyle, animatedHeaderStyle]}>
+      <Animated.Image
+        source={require("../assets/images/pokedex_header.png")}
+        style={[styles.headerImage, animatedHeaderStyle]}
+        resizeMode="cover"
+      />
+      <Animated.Image
+        source={require("../assets/images/profile_image.png")}
+        resizeMode="cover"
+        style={[
+          styles.imagePosition,
+          animatedProfileImageStyle,
+        ]}
+      />
     </Animated.View>
   );
 }
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    width: "100%",
+    position: "relative",
+  },
+  headerImage: {
+    ...StyleSheet.absoluteFillObject,
+    width: "100%",
+  },
+  imagePosition: {
+    position: "absolute",
+    left: 20,
+  },
+});
