958ba9d8b9e86db0b4b442c2aae0165842698827
rikuyu <rikuyu5884@gmail.com>
2025-04-17 19:04:09 +0900

    get location

---
+++ components/Residence.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+import * as Location from "expo-location";
+
+export default function Residence() {
+  const [location, setLocation] = useState<string>("unknown");
+
+  const getCurrentLocation = async () => {
+    let {status} = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      return;
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    const {latitude, longitude} = location.coords;
+    const geocode = await Location.reverseGeocodeAsync({latitude, longitude});
+    const {city, country} = geocode[0];
+    setLocation(`${country}/${city}`);
+  };
+
+  return (
+    <TouchableOpacity
+      style={styles.container}
+      onPress={() => getCurrentLocation()}
+      hitSlop={36}
+      activeOpacity={0.8}
+    >
+      <Ionicons name="location-outline" size={20} color={"#b1b1b1"}/>
+      <View style={{width: 4}}/>
+      <Text style={styles.text}>{location}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#000",
+    paddingHorizontal: 16,
+  },
+  text: {
+    fontSize: 14,
+    color: "#b1b1b1",
+  },
+});
