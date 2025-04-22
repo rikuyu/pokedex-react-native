1e272624f202f5ab16b3c0eb26378bf79aa2abfc
rikuyu <rikuyu5884@gmail.com>
2025-04-22 21:12:45 +0900

    impl Animated FlatList

--- app/(tabs)/mypage.tsx
+++ app/(tabs)/mypage.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback } from "react";
-import { ActivityIndicator, StyleSheet, Text, useWindowDimensions } from "react-native";
+import { ActivityIndicator, FlatList, StyleSheet, useWindowDimensions } from "react-native";
 import { useFocusEffect, useRouter } from "expo-router";
 import Animated, { useAnimatedScrollHandler, useSharedValue } from "react-native-reanimated";
 import MyPageHeader from "@/components/MyPageHeader";
@@ -11,9 +11,17 @@ import { useMyProfile } from "@/hooks/useMyProfile";
 import { ThemedView } from "@/components/ThemedView";
 import { SafeAreaView } from "react-native-safe-area-context";
 import PokemonToolItem from "@/components/PokemonToolItem";
-import ScrollView = Animated.ScrollView;
+import { useFetch } from "@/hooks/useFetch";
+import { BerryData } from "@/types/berry";
+import { fetchBerryList } from "@/services/fetchBerryList";
+import { useThemeColor } from "@/hooks/useThemeColor";
+import { darkTextColor, lightTextColor } from "@/constants/colors";
+import { ThemedText } from "@/components/ThemedText";
+
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList<BerryData>);
 
 export default function MyPage() {
+  const borderColor = useThemeColor({light: lightTextColor, dark: darkTextColor});
   const router = useRouter();
 
   const scrollY = useSharedValue(0);
@@ -24,7 +32,7 @@ export default function MyPage() {
   const {width} = useWindowDimensions();
   const imageSize = width / 5;
 
-  const {profile, loading, error, loadProfile} = useMyProfile();
+  const {profile, loading: l1, error: e1, loadProfile} = useMyProfile();
 
   useFocusEffect(
     useCallback(() => {
@@ -32,7 +40,9 @@ export default function MyPage() {
     }, []),
   );
 
-  if (loading) {
+  const {data: berryList, isLoading: l2, hasError: e2} = useFetch<BerryData[], void>(fetchBerryList);
+
+  if (l1 || l2) {
     return (
       <ThemedView style={styles.container}>
         <ActivityIndicator size="large"/>
@@ -40,10 +50,10 @@ export default function MyPage() {
     );
   }
 
-  if (error) {
+  if (e1 || e2) {
     return (
       <ThemedView style={styles.container}>
-        <Text>Error</Text>
+        <ThemedText>Error</ThemedText>
       </ThemedView>
     );
   }
@@ -56,25 +66,27 @@ export default function MyPage() {
           scrollOffset={scrollY}
           positionStyle={styles.headerPosition}
         />
-        <ScrollView
-          style={styles.listContainer}
+        <AnimatedFlatList
           onScroll={scrollHandler}
-        >
-          <EditButtonSection
-            onPress={() => router.push("/edit")}
-            height={imageSize / 2}
-          />
-          <BiographySection profile={profile}/>
-          <Residence/>
-          <ThemedView style={{height: 8}}/>
-          <Birthday/>
-          <ThemedView style={{height: 24}}/>
-          {Array.from({length: 10}).map((_, index) => {
-            return (
-              <PokemonToolItem key={index}/>
-            );
-          })}
-        </ScrollView>
+          scrollEventThrottle={16}
+          ListHeaderComponent={() => (
+            <>
+              <EditButtonSection
+                onPress={() => router.push("/edit")}
+                height={imageSize / 2}
+              />
+              <BiographySection profile={profile}/>
+              <Residence/>
+              <ThemedView style={{height: 8}}/>
+              <Birthday/>
+              <ThemedView style={{height: 12, borderBottomWidth: 0.5, borderBottomColor: borderColor}}/>
+            </>
+          )}
+          data={berryList}
+          keyExtractor={(_, index) => index.toString()}
+          renderItem={({item}) => <PokemonToolItem berry={item}/>}
+          style={styles.listContainer}
+        />
       </ThemedView>
     </SafeAreaView>
   );
@@ -85,7 +97,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#000",
   },
   headerPosition: {
     zIndex: 2,
