b6e2e3c90a5825325e5e68ed867847877e4a4700
rikuyu <rikuyu5884@gmail.com>
2025-04-25 00:18:42 +0900

    add PokemonToolItem component for displaying berry details

---
+++ components/PokemonToolItem.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { ThemedView } from "@/components/ThemedView";
+import { Image, StyleSheet } from "react-native";
+import { ThemedText } from "@/components/ThemedText";
+import { useThemeColor } from "@/hooks/useThemeColor";
+import { darkTextColor, lightTextColor } from "@/constants/colors";
+import { BerryData } from "@/types/berry";
+
+type Props = {
+  berry: BerryData;
+}
+
+export default function PokemonToolItem({berry}: Props) {
+  const borderColor = useThemeColor({light: lightTextColor, dark: darkTextColor});
+
+  return (
+    <ThemedView style={[styles.container, {borderColor}]}>
+      <Image
+        style={styles.image}
+        source={{uri: berry.image}}
+      />
+      <ThemedView style={{width: 12}}/>
+      <ThemedView style={styles.description}>
+        <ThemedText type={"size14Medium"}>{berry.name}</ThemedText>
+        <ThemedView style={{height: 4}}/>
+        <ThemedText type={"size12Normal"}>
+          {berry.description.replace(/[　\n]/g, "").split("。")[0]}
+        </ThemedText>
+      </ThemedView>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingVertical: 16,
+    borderBottomWidth: 0.5,
+  },
+  image: {
+    width: 32,
+    height: 32,
+  },
+  description: {
+    flex: 1,
+    flexDirection: "column",
+  },
+});
