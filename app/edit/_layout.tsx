0662ffe70da01d2df8690e631ac29c34fa498219
rikuyu <rikuyu5884@gmail.com>
2025-05-07 00:17:28 +0900

    delete unnecessary style

--- app/edit/_layout.tsx
+++ app/edit/_layout.tsx
@@ -2,7 +2,7 @@ import { Stack } from "expo-router";
 import { Platform } from "react-native";
 import IosBackButton from "@/components/IosBackButton";
 import React from "react";
-import { headerStyle, tabBarStyle } from "@/constants/colors";
+import { headerStyle } from "@/constants/colors";
 
 export default function EditLayout() {
   return (
@@ -10,7 +10,6 @@ export default function EditLayout() {
       screenOptions={{
         title: "Edit Profile",
         headerStyle,
-        tabBarStyle,
         headerTintColor: "#ffffff",
         headerLeft: () => Platform.OS === "ios" ? <IosBackButton iconColor={"white"}/> : null,
       }}
