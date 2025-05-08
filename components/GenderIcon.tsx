360823c70f092711da237b8f504f47510aa01b90
rikuyu <rikuyu5884@gmail.com>
2025-05-09 00:33:22 +0900

    gender icon colors

--- components/GenderIcon.tsx
+++ components/GenderIcon.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import { femalePink, maleBlue } from "@/constants/colors";
 
 export default function GenderIcon() {
   const isMale = Math.random() < 0.5;
   return (
     isMale ?
-      <MaterialIcons name="male" size={20} color="#68abfc"/>
+      <MaterialIcons name="male" size={20} color={maleBlue}/>
       :
-      <MaterialIcons name="female" size={20} color="#fcacac"/>
+      <MaterialIcons name="female" size={20} color={femalePink}/>
   );
 }
