76666dcd26d70afb1e6d596dd419ee8f7fc70d91
rikuyu <rikuyu5884@gmail.com>
2025-05-11 01:14:17 +0900

    fix edit title

--- app/edit/_layout.tsx
+++ app/edit/_layout.tsx
@@ -3,13 +3,14 @@ import { Platform } from "react-native";
 import IosBackButton from "@/components/IosBackButton";
 import React from "react";
 import { headerStyle } from "@/constants/colors";
+import { isJa } from "@/utils/i18n";
 
 export default function EditLayout() {
   console.log(`EXPO_PUBLIC_TEST=${process.env.EXPO_PUBLIC_TEST}`);
   return (
     <Stack
       screenOptions={{
-        title: "Edit Profile",
+        title: isJa ? "プロフィールの編集" : "Edit Profile",
         headerStyle,
         headerTintColor: "#ffffff",
         headerLeft: () => Platform.OS === "ios" ? <IosBackButton iconColor={"white"}/> : null,
