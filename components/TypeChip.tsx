834ec8bf1e4ef231bf98aa7ddcc01238a82aa459
rikuyu <rikuyu5884@gmail.com>
2025-05-12 00:02:41 +0900

    resolve lint error

--- components/TypeChip.tsx
+++ components/TypeChip.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { getColorIsLight } from "@/utils/getColorIsLight";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
