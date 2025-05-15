42a093f41e1ad0b9285a19688c142e376625e13c
rikuyu <rikuyu5884@gmail.com>
2025-05-16 00:49:13 +0900

    fix

--- components/GenderIcon.tsx
+++ components/GenderIcon.tsx
@@ -4,7 +4,5 @@ import { Mars, Venus } from "@tamagui/lucide-icons";
 
 export default function GenderIcon() {
   const isMale = Math.random() < 0.5;
-  return (
-    isMale ? <Mars size="$1" color={maleBlue} /> : <Venus size="$1" color={femalePink} />
-  );
+  return isMale ? <Mars size="$1" col={maleBlue} /> : <Venus size="$1" col={femalePink} />;
 }
