bdecc59847f9cd1836b78eb1e164c4ad4762e660
rikuyu <rikuyu5884@gmail.com>
2025-05-17 03:10:28 +0900

    GradientOrSolidBackground

--- components/GradientOrSolidBackground.tsx
+++ components/GradientOrSolidBackground.tsx
@@ -1,49 +1,28 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
-import { useThemeColor } from "@/hooks/useThemeColor";
-import { LinearGradient } from "expo-linear-gradient";
+import { LinearGradient } from "@tamagui/linear-gradient";
+import { View } from "tamagui";
+import { TypeColor } from "@/utils/typeData";
 
 type Props = {
-  colors: string[] | undefined;
+  colors: TypeColor[] | undefined;
   children: React.ReactElement | React.ReactElement[];
 }
 
 export default function GradientOrSolidBackground({colors, children}: Props) {
-  const fallbackColor = useThemeColor(undefined, "background");
-  const safeColors = colors?.length ? colors : [fallbackColor];
+  const safeColors: TypeColor[] = colors?.length ? colors : ["#ffffff"];
 
   if (safeColors.length >= 2) {
     const gradientColors = safeColors as [string, string, ...string[]];
     return (
-      <LinearGradient colors={gradientColors} style={styles.linearGradient}>
+      <LinearGradient colors={gradientColors} f={1.25} w={"100%"} jc={"center"} ai={"center"} bblr={40} bbrr={40}>
         {children}
       </LinearGradient>
     );
   } else {
     return (
-      <View
-        style={[
-          styles.linearGradient,
-          {
-            width: "100%",
-            backgroundColor: safeColors[0],
-          },
-        ]}
-      >
+      <View bg={safeColors[0]} f={1.25} w={"100%"} jc={"center"} ai={"center"} bblr={40} bbrr={40}>
         {children}
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  linearGradient: {
-    flex: 1.25,
-    width: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-    borderBottomLeftRadius: 40,
-    borderBottomRightRadius: 40,
-    overflow: "hidden",
-  },
-});
