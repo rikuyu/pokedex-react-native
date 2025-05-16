a04a0792262e06ce926ef51b2d2f29bbc962c9e3
rikuyu <rikuyu5884@gmail.com>
2025-05-17 00:05:18 +0900

    type chip

--- components/TypeChip.tsx
+++ components/TypeChip.tsx
@@ -1,31 +1,18 @@
 import React from "react";
-import { StyleSheet } from "react-native";
 import { getColorIsLight } from "@/utils/getColorIsLight";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+import { Chip } from "@/components/Chip";
+import { TypeColor } from "@/utils/typeData";
 
 type Props = {
   name: string;
-  color: string;
+  color: TypeColor;
 }
 
 export default function TypeChip({name, color}: Props) {
   const labelColor = getColorIsLight(color) ? "black" : "white";
   return (
-    <ThemedView style={styles.chip} lightColor={color} darkColor={color}>
-      <ThemedText type="size16Bold" lightColor={labelColor} darkColor={labelColor}>
-        {name}
-      </ThemedText>
-    </ThemedView>
+    <Chip bg={color}>
+      <Chip.Text col={labelColor}>{name}</Chip.Text>
+    </Chip>
   );
 }
-
-const styles = StyleSheet.create({
-  chip: {
-    width: 90,
-    height: 32,
-    borderRadius: 50,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
