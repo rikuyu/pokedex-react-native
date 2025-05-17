1710c6022c5d2555dacfbc50f383d2540bbfb85a
rikuyu <rikuyu5884@gmail.com>
2025-05-18 00:08:10 +0900

    StatItem & StatSection

--- components/StatItem.tsx
+++ components/StatItem.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useMemo } from "react";
-import { Animated, Easing, StyleSheet, Text, useAnimatedValue, useWindowDimensions, View } from "react-native";
-import { ThemedView } from "@/components/ThemedView";
-import { ThemedText } from "@/components/ThemedText";
+import { Animated, Easing, useAnimatedValue, useWindowDimensions } from "react-native";
+import { Text, View, XStack, ZStack } from "tamagui";
 
 type Props = {
   label: string;
@@ -31,70 +30,31 @@ export default function StatItem({label, value, color}: Props) {
   }, [value, barUnit, scaleAnim]);
 
   return (
-    <View style={styles.container}>
-      <ThemedView style={styles.labelContainer}>
-        <ThemedText type={"size16Medium"} numberOfLines={1} ellipsizeMode="tail">
+    <XStack w={"100%"} jc={"center"}>
+      <View f={1} als={"center"}>
+        <Text als={"center"} fos={16} fow={"400"} numberOfLines={1} ellipsizeMode="tail">
           {label}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={{width: 8}}/>
-      <ThemedView lightColor={"#e0e8f0"} darkColor={"#464646"} style={styles.statContainer}>
+        </Text>
+      </View>
+      <View w={8}/>
+      <ZStack f={4} bg={"$statBar"} h={24} br={50} ov={"hidden"}>
         <Animated.View
-          style={[
-            styles.statAnimBar,
-            {
-              backgroundColor: color,
-              transform: [{scaleX: scaleAnim}],
-              transformOrigin: "left",
-            },
-          ]}
+          style={[{
+            backgroundColor: color,
+            transform: [{scaleX: scaleAnim}],
+            transformOrigin: "left",
+            flex: 4,
+            position: "absolute",
+            top: 0,
+            left: 0,
+            bottom: 0,
+            width: 1,
+          }]}
         />
-        <View style={styles.statValueContainer}>
-          <Text style={styles.statValue}>{value}</Text>
+        <View jc={"center"} ai={"flex-start"}>
+          <Text ml={8} fow={"bold"} col={"white"}>{value}</Text>
         </View>
-      </ThemedView>
-    </View>
+      </ZStack>
+    </XStack>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: "row",
-    justifyContent: "center",
-    width: "100%",
-  },
-  labelContainer: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  statContainer: {
-    flex: 4,
-    height: 24,
-    borderRadius: 50,
-    position: "relative",
-    overflow: "hidden",
-  },
-  statAnimBar: {
-    flex: 4,
-    position: "absolute",
-    top: 0,
-    left: 0,
-    bottom: 0,
-    width: 1,
-  },
-  statValueContainer: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    justifyContent: "center",
-    alignItems: "flex-start",
-  },
-  statValue: {
-    color: "#fff",
-    fontWeight: "bold",
-    marginStart: 8,
-  },
-});
--- components/StatSection.tsx
+++ components/StatSection.tsx
@@ -1,41 +1,28 @@
 import React from "react";
-import { StyleSheet } from "react-native";
 import StatItem from "@/components/StatItem";
 import { getStatColor, getStatLabel } from "@/utils/statData";
 import { PokemonStat } from "@/types/pokemon";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
 import { i18nText } from "@/utils/i18n";
+import { Text, View, YStack } from "tamagui";
 
 export default function StatSection({stats}: { stats: PokemonStat[] | undefined }) {
   const safeStats = stats ?? [];
   return (
-    <ThemedView style={styles.container}>
-      <ThemedText type={"size16Medium"}>{i18nText("stat")}</ThemedText>
-      <ThemedView style={{height: 12}}/>
+    <YStack bg={"$background"} br={36} py={12} px={20} mx={12} ac={"center"}>
+      <Text fos={16} fow={"500"} als={"center"}>{i18nText("stat")}</Text>
+      <View h={12}/>
       {
         safeStats.flatMap((stat, index) => [
           <StatItem
             key={`stat-${index}`}
             label={getStatLabel(stat.name)}
             value={stat.status}
-            color={getStatColor(stat.name)}/>,
-          index !== safeStats.length - 1 && (
-            <ThemedText key={`spacer-${index}`} style={{height: 20}}/>
-          ),
+            color={getStatColor(stat.name)}
+          />,
+          index !== safeStats.length - 1 && <View key={`spacer-${index}`} style={{height: 20}}/>,
         ])
       }
-      <ThemedText style={{height: 20}}/>
-    </ThemedView>
+      <View h={20}/>
+    </YStack>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    borderRadius: 36,
-    paddingVertical: 12,
-    paddingHorizontal: 20,
-    marginHorizontal: 12,
-    alignItems: "center",
-  },
-});
