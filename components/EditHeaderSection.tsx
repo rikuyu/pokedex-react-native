37eec514a0d503a748a3976c5e55ec59bdd0566f
rikuyu <rikuyu5884@gmail.com>
2025-05-19 02:22:00 +0900

    EditHeaderSection

--- components/EditHeaderSection.tsx
+++ components/EditHeaderSection.tsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { Image, Pressable, StyleSheet, View } from "react-native";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import ThemeToggleButton from "@/components/ThemeToggleButton";
-import { ThemedView } from "@/components/ThemedView";
+import { Image, View, ZStack } from "tamagui";
 
 type Props = {
   imageSize: number,
@@ -13,107 +12,38 @@ type Props = {
 }
 
 export default function EditHeaderSection({imageSize, iconImg, setIcon, headerImg, setHeader}: Props) {
-  const imgStyle = {
-    width: imageSize,
-    height: imageSize,
-    borderRadius: imageSize / 2,
-  };
-
   const header = headerImg ? {uri: headerImg} : require("../assets/images/header_placeholder.png");
   const icon = iconImg ? {uri: iconImg} : require("../assets/images/profile_image.png");
 
   return (
-    <ThemedView style={styles.container}>
-      <Pressable
-        onPress={setHeader}
-        style={({pressed}) => [
-          styles.headerTouchable,
-          {opacity: pressed ? 0.9 : 1},
-        ]}
-      >
-        <View style={styles.touchableContent}>
-          <Image source={header} style={[styles.headerImage]} resizeMode="cover"/>
-          <ThemedView style={styles.blurHeader}>
-            <MaterialCommunityIcons
-              name="camera-plus-outline"
-              color="#fff"
-              size={32}
-            />
-          </ThemedView>
-          <ThemeToggleButton positionStyle={styles.themeBtn}/>
+    <ZStack h={130}>
+      <ZStack onPress={setHeader} pressStyle={{o: 0.9}} h={130}>
+        <Image source={header} w={"100%"} h={"100%"} objectFit="cover"/>
+        <View
+          bg={"#00000055"}
+          jc={"center"} ai={"center"}
+          t={0} b={0} r={0} l={0}
+          pos={"absolute"}
+        >
+          <MaterialCommunityIcons name="camera-plus-outline" color="#fff" size={32}/>
         </View>
-      </Pressable>
-      <Pressable
-        onPress={setIcon}
-        style={({pressed}) => [
-          styles.iconTouchable, {
-            bottom: imageSize / 2,
-            opacity: pressed ? 0.9 : 1,
-          },
-        ]}
-      >
-        <View style={styles.touchableContent}>
-          <Image source={icon} resizeMode="cover" style={[styles.border, imgStyle]}/>
-          <ThemedView style={[styles.blurIcon, imgStyle]}>
-            <MaterialCommunityIcons name="camera-plus-outline" color="#fff" size={28}/>
-          </ThemedView>
+        <ThemeToggleButton/>
+      </ZStack>
+      <ZStack onPress={setIcon} pressStyle={{o: 0.9}} b={imageSize / 2} l={16} pos={"absolute"}>
+        <Image source={icon} objectFit="cover" w={imageSize} h={imageSize} br={50}/>
+        <View
+          jc={"center"} ai={"center"}
+          w={imageSize} h={imageSize}
+          t={0} b={0} r={0} l={0}
+          pos={"absolute"}
+          bg={"#00000055"}
+          br={50}
+          bw={2}
+          boc={"black"}
+        >
+          <MaterialCommunityIcons name="camera-plus-outline" color="#fff" size={28}/>
         </View>
-      </Pressable>
-    </ThemedView>
+      </ZStack>
+    </ZStack>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    width: "100%",
-    height: 130,
-    position: "relative",
-  },
-  headerTouchable: {
-    ...StyleSheet.absoluteFillObject,
-    height: 130,
-    width: "100%",
-  },
-  touchableContent: {
-    position: "relative",
-    height: "100%",
-    width: "100%",
-  },
-  headerImage: {
-    ...StyleSheet.absoluteFillObject,
-    height: "100%",
-    width: "100%",
-  },
-  blurHeader: {
-    ...StyleSheet.absoluteFillObject,
-    height: "100%",
-    width: "100%",
-    backgroundColor: "#0000004D",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  themeBtn: {
-    position: "absolute",
-    top: 8,
-    right: 8,
-  },
-  iconTouchable: {
-    position: "absolute",
-    left: 16,
-  },
-  border: {
-    ...StyleSheet.absoluteFillObject,
-    height: "100%",
-    width: "100%",
-    borderWidth: 2,
-    borderColor: "#000",
-  },
-  blurIcon: {
-    ...StyleSheet.absoluteFillObject,
-    height: "100%",
-    width: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#00000033",
-  },
-});
