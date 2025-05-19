0a1333090d2888988c0a34dbae6af2a05d3d6f9a
rikuyu <rikuyu5884@gmail.com>
2025-05-20 00:59:40 +0900

    BookmarkPokemonImage

--- components/BookmarkPokemonImage.tsx
+++ components/BookmarkPokemonImage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { getPokemonDotImage } from "@/constants/endpoints";
 import ItemBadge from "@/components/ItemBadge";
-import { Image, View, YStack } from "tamagui";
+import { Image, View, YStack, ZStack } from "tamagui";
 
 type Props = {
   pokemonId: number;
@@ -11,13 +11,15 @@ type Props = {
 export default function BookmarkPokemonImage({pokemonId, itemSize}: Props) {
   const imageSize = itemSize - 12;
   return (
-    <View w={itemSize} ar={1} boc={"$color"} br={50} bw={1} ov={"hidden"} pos={"relative"} jc={"center"} ai={"center"} >
-      <YStack pos={"absolute"} t={0} l={0} r={0} b={0}>
+    <ZStack w={itemSize} ar={1} boc={"$color"} br={50} bw={1} ov={"hidden"}>
+      <YStack f={1}>
         <View f={1} bg={"$pokedexRed"}/>
         <View f={1}/>
       </YStack>
-      <Image h={imageSize} ar={1} source={{uri: getPokemonDotImage(pokemonId)}}/>
-      {Math.random() > 0.4 && <ItemBadge />}
-    </View>
+      <View f={1} jc={"center"} ai={"center"}>
+        <Image h={imageSize} ar={1} source={{uri: getPokemonDotImage(pokemonId)}}/>
+      </View>
+      {Math.random() > 0.4 && <ItemBadge/>}
+    </ZStack>
   );
 }
