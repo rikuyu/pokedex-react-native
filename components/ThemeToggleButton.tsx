da190fbfdc050358a5ad47e010a572113d0ae71c
rikuyu <rikuyu5884@gmail.com>
2025-05-21 04:37:19 +0900

    fix ThemeToggleButton

--- components/ThemeToggleButton.tsx
+++ components/ThemeToggleButton.tsx
@@ -1,17 +1,15 @@
 import React from "react";
-import { ViewStyle } from "react-native/Libraries/StyleSheet/StyleSheetTypes";
 import { useAppTheme } from "@/utils/ThemeContext";
 import { Moon, Sun } from "@tamagui/lucide-icons";
 import { View } from "tamagui";
 
-export default function ThemeToggleButton({positionStyle}: { positionStyle: ViewStyle }) {
+export default function ThemeToggleButton() {
   const {theme, setTheme} = useAppTheme();
 
   return (
     <View
       onPress={() => setTheme(theme === "light" ? "dark" : "light")}
       pressStyle={{o: 0.7}}
-      style={positionStyle}
       bg={"rgba(100,98,98,0.6)"}
       ai={"center"}
       jc={"center"}
@@ -19,6 +17,9 @@ export default function ThemeToggleButton({positionStyle}: { positionStyle: View
       ar={1}
       h={"$3.5"}
       w={"$3.5"}
+      pos={"absolute"}
+      t={8}
+      r={8}
     >
       {theme === "light" ? <Moon size="$1" col="white"/> : <Sun size="$1" col="white"/>}
     </View>
