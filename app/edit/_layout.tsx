774dcff748e72653f107ec9e157d8305e9f8bd7a
rikuyu <rikuyu5884@gmail.com>
2025-05-24 13:57:49 +0900

    add expo-device

--- app/edit/_layout.tsx
+++ app/edit/_layout.tsx
@@ -4,9 +4,11 @@ import IosBackButton from "@/components/IosBackButton";
 import React from "react";
 import { headerStyle } from "@/constants/colors";
 import { i18nText } from "@/utils/i18n";
+import * as Device from "expo-device";
 
 export default function EditLayout() {
   console.log(`EXPO_PUBLIC_TEST=${process.env.EXPO_PUBLIC_TEST}`);
+  console.log(`modelName:${Device.modelName} osName:${Device.osName} osVersion:${Device.osVersion}`);
 
   return (
     <Stack
