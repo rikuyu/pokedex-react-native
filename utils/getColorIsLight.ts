d0e12581ac3f61ce02679be41d05feadb3268b67
rikuyu <rikuyu5884@gmail.com>
2025-05-25 16:55:57 +0900

    rename

--- utils/getColorIsLight.ts
+++ utils/getColorIsLight.ts
@@ -3,11 +3,11 @@ export const getColorIsLight = (bgColor: string): boolean => {
 
   const color = bgColor.startsWith("#") ? bgColor : `#${bgColor}`;
 
-  const r = parseInt(color.slice(1, 3), 16) || 0;
-  const g = parseInt(color.slice(3, 5), 16) || 0;
-  const b = parseInt(color.slice(5, 7), 16) || 0;
+  const red = parseInt(color.slice(1, 3), 16) || 0;
+  const green = parseInt(color.slice(3, 5), 16) || 0;
+  const blue = parseInt(color.slice(5, 7), 16) || 0;
 
-  const luminance = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+  const luminance = (0.299 * red + 0.587 * green + 0.114 * blue) / 255;
 
   return luminance > 0.8;
 };
