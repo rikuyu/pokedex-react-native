dc99be2ac5618fc05136c58bb87bac9a26f9315b
rikuyu <rikuyu5884@gmail.com>
2025-05-26 02:47:03 +0900

    DebugItem

---
+++ components/DebugItem.tsx
@@ -0,0 +1,23 @@
+import { styled, Text, withStaticProperties, XStack } from "tamagui";
+
+const DebugItemFrame = styled(XStack, {
+  name: "DebugItem",
+  ai: "center",
+  jc: "flex-start",
+  bbw: 0.5,
+  boc: "$color",
+  bg: "$background",
+  py: 12,
+  px: 20,
+  w: "100%",
+});
+
+const DebugItemText = styled(Text, {
+  name: "DebugItemText",
+  fos: 14,
+  col: "$color",
+});
+
+export const DebugItem = withStaticProperties(DebugItemFrame, {
+  Text: DebugItemText,
+});
