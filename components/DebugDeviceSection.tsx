939b580229d6f44429e5cca62a7a2890b4b92da6
rikuyu <rikuyu5884@gmail.com>
2025-05-27 19:27:18 +0900

    debug device

---
+++ components/DebugDeviceSection.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { DebugTitle } from "@/components/DebugTitle";
+import { DebugItem } from "@/components/DebugItem";
+import * as Device from "expo-device";
+
+export default function DebugDeviceSection() {
+  return (
+    <>
+      <DebugTitle>
+        <DebugTitle.Text>Device</DebugTitle.Text>
+      </DebugTitle>
+      <DebugItem>
+        <DebugItem.Text>model: {Device.modelName}, os:{Device.osName} {Device.osVersion}, brand: {Device.brand}</DebugItem.Text>
+      </DebugItem>
+    </>
+  );
+}
