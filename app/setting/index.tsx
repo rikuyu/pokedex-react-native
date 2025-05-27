348c051e830465b61e9d8d035f10e30068840d50
rikuyu <rikuyu5884@gmail.com>
2025-05-27 19:36:16 +0900

    add debug sections

--- app/setting/index.tsx
+++ app/setting/index.tsx
@@ -1,11 +1,20 @@
 import React from "react";
-import { ScrollView } from "tamagui";
+import { ScrollView, View } from "tamagui";
 import DebugNotificationSection from "@/components/DebugNotificationSection";
+import DebugBatterySection from "@/components/DebugBatterySection";
+import DebugDeviceSection from "@/components/DebugDeviceSection";
+import DebugNotFoundSection from "@/components/DebugNotFoundSection";
 
 export default function Index() {
   return (
     <ScrollView f={1} bg={"$background"} py={14}>
       <DebugNotificationSection/>
+      <View h={20}/>
+      <DebugBatterySection/>
+      <View h={20}/>
+      <DebugDeviceSection/>
+      <View h={20}/>
+      <DebugNotFoundSection />
     </ScrollView>
   );
 };
