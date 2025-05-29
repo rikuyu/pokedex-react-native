48a5b4a23db1d95b9adb5bab381b64310b3ee45f
rikuyu <rikuyu5884@gmail.com>
2025-05-29 22:31:11 +0900

    fix DebugNotificationSection

--- components/DebugNotificationSection.tsx
+++ components/DebugNotificationSection.tsx
@@ -4,6 +4,7 @@ import { DebugItem } from "@/components/DebugItem";
 import { sendLocalNotification, sendPushNotification } from "@/services/notifications";
 import { useNotification } from "@/context/NotificationContext";
 import * as Clipboard from "expo-clipboard";
+import { View } from "tamagui";
 
 export default function DebugNotificationSection() {
   const {expoPushToken, notification, error} = useNotification();
@@ -30,10 +31,15 @@ export default function DebugNotificationSection() {
         <DebugTitle.Text>Notification</DebugTitle.Text>
       </DebugTitle>
       <DebugItem onPress={debugRemoteNotification}>
-        <DebugItem.Text>Push：{expoPushToken || "null"}</DebugItem.Text>
+        <DebugItem.Text>
+          Push：
+          {error ? <DebugItem.Text col="$pokedexRed">error</DebugItem.Text> : expoPushToken || "null"}
+        </DebugItem.Text>
       </DebugItem>
       <DebugItem onPress={debugLocalNotification}>
         <DebugItem.Text>Local リダイレクト機能</DebugItem.Text>
+        <View w={8}/>
+        {error && <DebugItem.Text col="$pokedexRed">error</DebugItem.Text>}
       </DebugItem>
     </>
   );
