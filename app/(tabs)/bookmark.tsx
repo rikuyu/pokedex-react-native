e06bfe56fed24013915ffc43928d63bf7a50dcf9
rikuyu <rikuyu5884@gmail.com>
2025-05-31 00:13:32 +0900

    add EmptyView

--- app/(tabs)/bookmark.tsx
+++ app/(tabs)/bookmark.tsx
@@ -1,23 +1,22 @@
 import React from "react";
-import { FlatList } from "react-native";
+import { FlatList, Pressable } from "react-native";
 import BookmarkPokemon from "@/components/BookmarkPokemon";
 import { useRouter } from "expo-router";
 import { useBookmarkPokemon } from "@/hooks/useDrizzleClient";
-import { View } from "tamagui";
+import { Text, View, YStack } from "tamagui";
 import FullScreenLoadingIndicator from "@/components/FullScreenLoadingIndicator";
 import FullScreenErrorView from "@/components/FullScreenErrorView";
+import { i18nText } from "@/utils/i18n";
 
 export default function Bookmark() {
   const router = useRouter();
   const {data: bookmarks, isLoading, isError} = useBookmarkPokemon();
 
-  if (isLoading) {
-    return <FullScreenLoadingIndicator/>;
-  }
+  if (isLoading) return <FullScreenLoadingIndicator/>;
 
-  if (isError) {
-    return <FullScreenErrorView/>;
-  }
+  if (isError) return <FullScreenErrorView/>;
+
+  if (bookmarks?.length === 0) return <EmptyView onPress={() => router.push("/")}/>;
 
   return (
     <View f={1} ai={"center"} jc={"center"} bg={"$background"}>
@@ -40,3 +39,18 @@ export default function Bookmark() {
     </View>
   );
 };
+
+const EmptyView = ({onPress}: { onPress: () => void }) => (
+  <YStack f={1} ai={"center"} jc={"center"} bg={"$background"}>
+    <Text fos={14}>{i18nText("bookmarkEmptyTitle")}</Text>
+    <View h={20}/>
+    <Pressable onPress={onPress}>
+      <View
+        px={12} py={8} bg={"$background"}
+        ai={"center"} jc={"center"}
+        br={999} bw={1} boc={"$color"}>
+        <Text>{i18nText("bookmarkEmptyBtnLabel")}</Text>
+      </View>
+    </Pressable>
+  </YStack>
+);
